d93fd1055c8cbe3155b6ddd5c2846d6c4a0dccda
Paul Buser <buser.paul@gmail.com>
2021-04-26 17:46:53 -0400

    Update Arch dependencies. Change local address to "minty".

--- web/conf/ConfigDev.elm
+++ web/conf/ConfigDev.elm
@@ -2,7 +2,7 @@ module Config exposing (config)
 
 
 config =
-    { sendServerAddress = "ws://192.168.86.116:8080/send"
-    , listenServerAddress = "ws://192.168.86.116:8080/listen"
+    { sendServerAddress = "ws://minty:8080/send"
+    , listenServerAddress = "ws://minty:8080/listen"
     , motionScaleFactor = 1 / 20
     }
